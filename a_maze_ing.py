941a6c95bbe130b1dcd38fbb2e93e62eff150000
lilith-th00 <massyliaaaa7@gmail.com>
2026-02-01 20:27:32 +0100

    Add error handling and main function for maze program
    Implement main function to handle file input and error management.

---
+++ a_maze_ing.py
@@ -0,0 +1,31 @@
+import sys
+import config_validation
+import algo
+import maze
+""""
+Your program must handle all errors gracefully: invalid configuration,
+file not found, bad syntax, impossible maze parameters, etc.
+It must never crash unexpectedly, and must always provide a clear
+error message to the user.
+"""
+
+
+def main():
+    args = sys.argv[1:]
+    if args.__len__() > 1:
+        print("Too many files , make sure to enter one file !!")
+    elif args.__len__() < 1:
+        print("You forgot to mention the config file !!")
+    else:
+        try:
+            with open(sys.argv[1], 'r') as file:
+                content = file.read()
+                data = config_validation.validation(content)
+                maze = algo.dfs(data)
+                maze.display(maze)
+        except (FileNotFoundError, config_validation.ErrorInConfigFile) as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    main()
